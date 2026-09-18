874939e1510f296336d71d0ce321eadc1e7df092
SStevenZ <2143emilio@gmail.com>
2026-09-18 16:04:43 -0500

    al fin url valio Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let reconnectAttempts = 0;
 const maxReconnectAttempts = 5;
 let reconnectTimeout = null;
 
-const streamUrl = "https://giss.tv:666/arturokm26.ogg";
+const streamUrl = "https://eu8.fastcast4u.com/stream/willy/stream";
 const playButton = document.getElementById('playButton');
 const playIcon = document.getElementById('playIcon');
 const volumeSlider = document.getElementById('volumeSlider');
