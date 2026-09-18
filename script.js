4f83057fef4c52a6491eebe4b60a113897231d70
SStevenZ <2143emilio@gmail.com>
2026-09-18 16:17:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let reconnectAttempts = 0;
 const maxReconnectAttempts = 5;
 let reconnectTimeout = null;
 
-const streamUrl = "http://eu8.fastcast4u.com:4152/;";
+const streamUrl = "https://eu8.fastcast4u.com/stream/willy/stream.pls";
 const playButton = document.getElementById('playButton');
 const playIcon = document.getElementById('playIcon');
 const volumeSlider = document.getElementById('volumeSlider');
