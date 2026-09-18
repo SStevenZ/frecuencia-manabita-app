367971aa34e3882626e34d74f7044f6570f43ecd
SStevenZ <2143emilio@gmail.com>
2026-09-18 14:49:45 -0500

    prueba url Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let reconnectAttempts = 0;
 const maxReconnectAttempts = 5;
 let reconnectTimeout = null;
 
-const streamUrl = "https://corsproxy.io/?url=http://78.129.241.110:3825/stream";
+const streamUrl = "https://giss.tv:666/arturokm26.ogg";
 const playButton = document.getElementById('playButton');
 const playIcon = document.getElementById('playIcon');
 const volumeSlider = document.getElementById('volumeSlider');
