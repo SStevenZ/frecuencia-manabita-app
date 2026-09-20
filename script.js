ff8ec76d1adfae1babb23fa61ced8e35133458a2
SStevenZ <2143emilio@gmail.com>
2026-09-20 14:08:31 -0500

    GISS.TV CAMBIO DE SERVIDOR GRATUITO Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let reconnectAttempts = 0;
 const maxReconnectAttempts = 5;
 let reconnectTimeout = null;
 
-const streamUrl = "https://eu8.fastcast4u.com/proxy/willy/stream";
+const streamUrl = "https://giss.tv:667/frecuenciamanabi.mp3";
 const playButton = document.getElementById('playButton');
 const playIcon = document.getElementById('playIcon');
 const volumeSlider = document.getElementById('volumeSlider');
